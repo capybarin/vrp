7204880c65f3d6e29542d96da3873c8479adf900
capybarin <vladibzd@gmail.com>
2021-02-08 00:37:10 +0200

    Update VrpController.java

--- src/main/java/com/diplom/vrp/controllers/VrpController.java
+++ src/main/java/com/diplom/vrp/controllers/VrpController.java
@@ -1,10 +1,12 @@
 package com.diplom.vrp.controllers;
 
 import com.diplom.vrp.utils.MultipleTimeWindowSolution;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class VrpController {
 
     @PostMapping(path = "/solve", consumes = "application/json", produces = "application/json")
